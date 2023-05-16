d8dcec9fdb26a05a0137d665cb93368208078ec4
“AratiG“ <aratinalla@gmail.com>
2023-05-16 09:44:42 +0530

    Added: Code to check employee present or absent

--- src/com/day3practiceproblems/EmployeeWage.java
+++ src/com/day3practiceproblems/EmployeeWage.java
@@ -3,5 +3,15 @@ package com.day3practiceproblems;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage problem...");
+        empPresentOrAbsent();
     }
+    public static void empPresentOrAbsent(){
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if(empCheck==IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+    }
+
 }
