1d160ad5dc4cbc342a11341e2186e5fb48a9c4c2
“AratiG“ <aratinalla@gmail.com>
2023-05-16 17:42:26 +0530

    Added: Code to print employee daily wage

--- src/com/day3practiceproblems/EmployeeWage.java
+++ src/com/day3practiceproblems/EmployeeWage.java
@@ -4,6 +4,7 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage problem...");
         empPresentOrAbsent();
+        employeeDailyWage();
     }
     public static void empPresentOrAbsent(){
         int IS_FULL_TIME = 1;
@@ -12,6 +13,12 @@ public class EmployeeWage {
             System.out.println("Employee is present");
         else
             System.out.println("Employee is absent");
+    }
+    public static void employeeDailyWage(){
+        int hrs_per_day = 8;
+        int wage_per_hr = 20;
+        int daily_wage = hrs_per_day * wage_per_hr;
+        System.out.println("Daily wage of employee is :"+daily_wage);
     }
 
 }
